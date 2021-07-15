0ba53413a0c4303d03fa8dc1abf0eb0f598779d7
Farheen khan <farheenfirdous12101999@gmail.com>
2021-07-15 09:28:15 -0700

    THA16

---
+++ react-tha16/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+   
+    return (
+        <div className="card">
+            <img src="https://images.hindustantimes.com/img/2021/06/29/550x309/m3_1624953455100_1624953498280.PNG" alt="meme" />
+        </div>
+    )
+}
+
+export default Card;
