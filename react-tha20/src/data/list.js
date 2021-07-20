e7810538d8fa718f346b7e662c072adc61806df2
Farheen khan <farheenfirdous12101999@gmail.com>
2021-07-20 09:59:58 -0700

    tha20✔

---
+++ react-tha20/src/data/list.js
@@ -0,0 +1,42 @@
+export const List = [
+    {
+      id: 1,
+      food: "Pizza 🍕",
+      calorie: 59,
+    },
+    {
+      id: 2,
+      food: "Burger 🍔",
+      calorie: 500,
+    },
+    {
+      id: 3,
+      food: "Coffee ☕",
+      calorie: 59,
+    },
+    {
+      id: 4,
+      food: "French Fries 🍟",
+      calorie: 59,
+    },
+    {
+      id: 5,
+      food: "Beer 🍻",
+      calorie: 200,
+    },
+    {
+      id: 6,
+      food: "Champaigne 🍾",
+      calorie: 179,
+    },
+    {
+      id: 7,
+      food: "Noodles 🍜",
+      calorie: 220,
+    },
+    {
+      id: 8,
+      food: "Coding 💻",
+      calorie: 0,
+    },
+  ];
